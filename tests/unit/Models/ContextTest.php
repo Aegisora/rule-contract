34249320c2c8735e09b23f07505dfacfcd398f21
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-12 19:58:19 +0400

    [AEGISORA-1] Implemented assertContextDataEqualsExpected.

--- tests/unit/Models/ContextTest.php
+++ tests/unit/Models/ContextTest.php
@@ -2,8 +2,15 @@
 
 namespace Aegisora\RuleContract\tests\unit\Models;
 
+use Aegisora\RuleContract\Models\Context;
 use PHPUnit\Framework\TestCase;
 
 class ContextTest extends TestCase
 {
+    private static function assertContextDataEqualsExpected(
+        Context $actual,
+        array $expected
+    ): void {
+        self::assertEquals($expected['value'], $actual->getValue());
+    }
 }
