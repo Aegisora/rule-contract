24b3f57971559855646eb9db716576401edcbfdd
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-12 20:01:05 +0400

    [AEGISORA-1] Implemented test function.

--- tests/unit/Models/ContextTest.php
+++ tests/unit/Models/ContextTest.php
@@ -7,6 +7,31 @@ use PHPUnit\Framework\TestCase;
 
 class ContextTest extends TestCase
 {
+    /**
+     * @dataProvider getContextProvidedData
+     */
+    public function testCreate(
+        array $actual,
+        array $expected
+    ): void {
+        self::assertContextDataEqualsExpected(Context::create(...array_values($actual)), $expected);
+    }
+
+    /**
+     * @dataProvider getContextProvidedData
+     */
+    public function testNewCreate(
+        array $actual,
+        array $expected
+    ): void {
+        self::assertContextDataEqualsExpected(new Context(...array_values($actual)), $expected);
+    }
+
+    public static function getContextProvidedData(): array
+    {
+        return [];
+    }
+
     private static function assertContextDataEqualsExpected(
         Context $actual,
         array $expected
