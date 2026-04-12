ca5b52de8d186e814c049cd772f2823d6d1d23c5
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-12 15:11:59 +0400

    [AEGISORA-1] Covered: DefaultCreate.

--- tests/unit/Exceptions/RuleExecutionExceptionTest.php
+++ tests/unit/Exceptions/RuleExecutionExceptionTest.php
@@ -2,8 +2,33 @@
 
 namespace Aegisora\RuleContract\tests\unit\Exceptions;
 
+use Aegisora\RuleContract\Exceptions\RuleExecutionException;
 use PHPUnit\Framework\TestCase;
 
 class RuleExecutionExceptionTest extends TestCase
 {
+    private const TEST_VALUE_RULE_CLASS_NAME = 'test_value_rule_class_name';
+
+    public function testDefaultCreate(): void
+    {
+        self::assertExceptionDataEqualsExpected(
+            new RuleExecutionException(self::TEST_VALUE_RULE_CLASS_NAME),
+            [
+                'ruleClassName' => self::TEST_VALUE_RULE_CLASS_NAME,
+                'message' => '',
+                'code' => 0,
+                'previous' => null,
+            ],
+        );
+    }
+
+    private static function assertExceptionDataEqualsExpected(
+        RuleExecutionException $actualException,
+        array $expectedData
+    ): void {
+        self::assertEquals($expectedData['ruleClassName'], $actualException->getRuleClassName());
+        self::assertEquals($expectedData['message'], $actualException->getMessage());
+        self::assertEquals($expectedData['code'], $actualException->getCode());
+        self::assertEquals($expectedData['previous'], $actualException->getPrevious());
+    }
 }
