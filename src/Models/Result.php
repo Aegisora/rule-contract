3035255ce26410d0c4894ae370eb09bac4158383
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-14 20:40:25 +0400

    [AEGISORA-1] Implemented attributes.

--- src/Models/Result.php
+++ src/Models/Result.php
@@ -4,4 +4,24 @@ namespace Aegisora\RuleContract\Models;
 
 class Result
 {
+    private bool $isValid;
+    private ?string $failedRuleCode;
+
+    public function __construct(
+        bool $isValid,
+        ?string $failedRuleCode = null
+    ) {
+        $this->isValid = $isValid;
+        $this->failedRuleCode = $failedRuleCode;
+    }
+
+    public function isValid(): bool
+    {
+        return $this->isValid;
+    }
+
+    public function getFailedRuleCode(): ?string
+    {
+        return $this->failedRuleCode;
+    }
 }
