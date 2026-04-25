33071f9a137e63cbcac747e6a49a246dc6b54a2b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-25 16:56:33 +0400

    [AEGISORA-1] Covered create.

--- tests/Unit/Models/ResultTest.php
+++ tests/Unit/Models/ResultTest.php
@@ -7,6 +7,62 @@ use PHPUnit\Framework\TestCase;
 
 class ResultTest extends TestCase
 {
+    /**
+     * @dataProvider getResultProvidedData
+     */
+    public function testCreateNew(
+        array $actual,
+        array $expected
+    ): void {
+        self::assertResultDataEqualsExpected(new Result(...array_values($actual)), $expected);
+    }
+
+    public static function getResultProvidedData(): array
+    {
+        return [
+            'is valid - true, failed rule code - null' => [
+                'actual' => [
+                    'isValid' => true,
+                    'failedRuleCode' => null,
+                ],
+                'expected' => [
+                    'isValid' => true,
+                    'failedRuleCode' => null,
+                ],
+            ],
+            'is valid - false, failed rule code - null' => [
+                'actual' => [
+                    'isValid' => false,
+                    'failedRuleCode' => null,
+                ],
+                'expected' => [
+                    'isValid' => false,
+                    'failedRuleCode' => null,
+                ],
+            ],
+            'is valid - false, failed rule code - not empty string' => [
+                'actual' => [
+                    'isValid' => false,
+                    'failedRuleCode' => 'foo',
+                ],
+                'expected' => [
+                    'isValid' => false,
+                    'failedRuleCode' => 'foo',
+                ],
+            ],
+            'is valid - false, failed rule code - empty string' => [
+                'actual' => [
+                    'isValid' => false,
+                    'failedRuleCode' => '',
+                ],
+                'expected' => [
+                    'isValid' => false,
+                    'failedRuleCode' => '',
+                ],
+            ],
+        ];
+    }
+
     private static function assertResultDataEqualsExpected(
         Result $actual,
         array $expected
