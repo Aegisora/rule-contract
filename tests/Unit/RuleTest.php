e5b86dccdb798125b1336b953628db1717777ade
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-25 17:42:14 +0400

    [AEGISORA-1] Covered: GetDefaultInvalidResult.

--- tests/Unit/RuleTest.php
+++ tests/Unit/RuleTest.php
@@ -1,9 +1,18 @@
 <?php
 
-namespace Aegisora\RuleContract\tests\Unit;
+namespace Aegisora\RuleContract\Tests\Unit;
 
+use Aegisora\RuleContract\Models\Context;
 use PHPUnit\Framework\TestCase;
 
 class RuleTest extends TestCase
 {
+    public function testGetDefaultInvalidResult(): void
+    {
+        $rule = new DefaultInvalidResultTestRule();
+        $result = $rule->validate(Context::create(null));
+
+        $this->assertFalse($result->isValid());
+        $this->assertEquals('default_invalid_result_test_rule', $result->getFailedRuleCode());
+    }
 }
