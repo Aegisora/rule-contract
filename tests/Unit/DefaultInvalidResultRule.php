53e803f0f5a5f15d61f71fc57b1cc5fcf341552b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-25 17:28:53 +0400

    [AEGISORA-1] Implemented DefaultInvalidResultRule for test.

---
+++ tests/Unit/DefaultInvalidResultRule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aegisora\RuleContract\tests\Unit;
+
+use Aegisora\RuleContract\Models\Context;
+use Aegisora\RuleContract\Models\Result;
+use Aegisora\RuleContract\Rule;
+
+class DefaultInvalidResultRule extends Rule
+{
+    protected function executeValidate(Context $context): Result
+    {
+        return $this->getDefaultInvalidResult();
+    }
+}
