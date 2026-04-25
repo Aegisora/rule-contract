926be6c6b6f8a3ccf2cb9c47e9070a209311395d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-25 17:13:53 +0400

    [AEGISORA-1] Implemented validate.

--- src/Rule.php
+++ src/Rule.php
@@ -2,6 +2,32 @@
 
 namespace Aegisora\RuleContract;
 
+use Aegisora\RuleContract\Exceptions\RuleException;
+use Aegisora\RuleContract\Exceptions\RuleExecutionException;
+use Aegisora\RuleContract\Models\Context;
+use Aegisora\RuleContract\Models\Result;
+use Throwable;
+
 abstract class Rule implements RuleInterface
 {
+    /**
+     * @throws RuleException
+     */
+    abstract protected function executeValidate(Context $context): Result;
+
+    public function validate(Context $context): Result
+    {
+        try {
+            return $this->executeValidate($context);
+        } catch (RuleException $exception) {
+            throw $exception;
+        } catch (Throwable $exception) {
+            throw new RuleExecutionException(
+                static::class,
+                $exception->getMessage(),
+                $exception->getCode(),
+                $exception
+            );
+        }
+    }
 }
