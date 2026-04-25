fef93025527cb22944b82978a3c3a6431264befa
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-04-25 17:47:24 +0400

    [AEGISORA-1] Covered: InvalidRuleContextException.

--- tests/Unit/RuleTest.php
+++ tests/Unit/RuleTest.php
@@ -2,11 +2,21 @@
 
 namespace Aegisora\RuleContract\Tests\Unit;
 
+use Aegisora\RuleContract\Exceptions\InvalidRuleContextException;
 use Aegisora\RuleContract\Models\Context;
 use PHPUnit\Framework\TestCase;
 
 class RuleTest extends TestCase
 {
+    public function testInvalidRuleContextException(): void
+    {
+        $rule = new InvalidRuleContextExceptionTestRule();
+
+        $this->expectException(InvalidRuleContextException::class);
+
+        $rule->validate(Context::create(null));
+    }
+
     public function testGetDefaultInvalidResult(): void
     {
         $rule = new DefaultInvalidResultTestRule();
