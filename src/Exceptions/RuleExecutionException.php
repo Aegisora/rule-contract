8a64c09a496b5ab89c3e150bf378bd982808728b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-14 17:54:12 +0400

    [AEGISORA-68752625] Fixed.

--- src/Exceptions/RuleExecutionException.php
+++ src/Exceptions/RuleExecutionException.php
@@ -12,7 +12,7 @@ class RuleExecutionException extends RuleException
         string $ruleClassName,
         string $message = "",
         int $code = 0,
-        Throwable $previous = null
+        ?Throwable $previous = null
     ) {
         parent::__construct($message, $code, $previous);
 
